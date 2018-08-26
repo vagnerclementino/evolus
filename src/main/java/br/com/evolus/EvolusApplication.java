fb1aebc730e2ac933e12bab402e30700e3a4aabb
Vagner Clementino <vagner.clementino@gmail.com>
2018-08-26 16:14:12 -0300

    Alteração no mapeamento do hibernate do relacionamento paciente -> convenio

--- src/main/java/br/com/evolus/EvolusApplication.java
+++ src/main/java/br/com/evolus/EvolusApplication.java
@@ -3,7 +3,6 @@
 package br.com.evolus;
 
 import java.util.Arrays;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -30,21 +29,22 @@ public class EvolusApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
+		//Criação dos convêncios que da base de teste
 		Convenio convenio1 = new Convenio(null, "11111111111", "UNIMED");
 		Convenio convenio2 = new Convenio(null,"000000000000" , "PROMED");
 		
+		//Criação dos pacientes da base de teste
 		Paciente paciente1 = new Paciente(null, "Vagner Clementino dos Santos", convenio1);
-		
 		Paciente paciente2 = new Paciente(null, "Andreza Vieira Lelis da Silva", convenio2);
-
 		Paciente paciente3 = new Paciente(null, "Maria de Lourdes Candido dos Santos", convenio1);
 
+		//Vinculando os pacientes aos respectivos convênios
 		convenio1.getPacientes().addAll(Arrays.asList(paciente1,paciente3));
-		
 		convenio2.getPacientes().add(paciente2);
 		
+		//Persistindo os os convênios e os pacientes
 		convenioRepository.saveAll(Arrays.asList(convenio1,convenio2));
-		pacienteRepository.saveAll(Arrays.asList(paciente1,paciente2));
+		pacienteRepository.saveAll(Arrays.asList(paciente1,paciente2, paciente3));
 				
 	}
 }
